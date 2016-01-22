13b04380cf5a1234b92e7bb335cba68b87872b22
Galileo Guzmán <galileoguzman@gmail.com>
2016-01-22 17:37:01 -0600

    Reachability on delegate

--- CheckInternetConn/AppDelegate.h
+++ CheckInternetConn/AppDelegate.h
@@ -7,6 +7,9 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "Reachability.h"
+
+#define stHost      @"https://smartplace.mx"
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
--- CheckInternetConn/AppDelegate.m
+++ CheckInternetConn/AppDelegate.m
@@ -16,7 +16,29 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+
+    Reachability* reach = [Reachability reachabilityWithHostname:stHost];
+    
+    // Set the blocks
+    reach.reachableBlock = ^(Reachability*reach)
+    {
+        // keep in mind this is called on a background thread
+        // and if you are updating the UI it needs to happen
+        // on the main thread, like this:
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            NSLog(@"REACHABLE!");
+        });
+    };
+    
+    reach.unreachableBlock = ^(Reachability*reach)
+    {
+        NSLog(@"UNREACHABLE!");
+    };
+    
+    // Start the notifier, which will cause the reachability object to retain itself!
+    [reach startNotifier];
+    
     return YES;
 }
 
