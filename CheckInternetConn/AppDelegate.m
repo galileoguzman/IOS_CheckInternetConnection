430721af946a94bcd2cc74e3c68bd3df67ec2d50
Galileo Guzmán <galileoguzman@gmail.com>
2016-01-22 18:17:37 -0600

    Send alert when user does not has internet connection

--- CheckInternetConn/AppDelegate.m
+++ CheckInternetConn/AppDelegate.m
@@ -16,24 +16,18 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    //[[NSNotificationCenter defaultCenter] postNotificationName:@"refreshView" object:nil];
 
     Reachability* reach = [Reachability reachabilityWithHostname:stHost];
     
-    // Set the blocks
-    reach.reachableBlock = ^(Reachability*reach)
-    {
-        // keep in mind this is called on a background thread
-        // and if you are updating the UI it needs to happen
-        // on the main thread, like this:
-        
-        dispatch_async(dispatch_get_main_queue(), ^{
-            NSLog(@"REACHABLE!");
-        });
-    };
     
     reach.unreachableBlock = ^(Reachability*reach)
     {
-        NSLog(@"UNREACHABLE!");
+        dispatch_async(dispatch_get_main_queue(), ^{
+            NSLog(@"UNREACHABLE!");
+            [[NSNotificationCenter defaultCenter] postNotificationName:@"refreshView" object:nil];
+        });
     };
     
     // Start the notifier, which will cause the reachability object to retain itself!
--- CheckInternetConn/Home.m
+++ CheckInternetConn/Home.m
@@ -17,11 +17,19 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(refreshView:) name:@"refreshView" object:nil];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)refreshView:(NSNotification *) notification{
+    UIAlertView *a = [[UIAlertView alloc] initWithTitle:@"Error" message:@"We don't have an internet connection!!! " delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"Accept", nil];
+    
+    [a show];
 }
 
 @end
